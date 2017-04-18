2466f9e40d8d7790ad556151fb25f90c4b55d21e
walley <walley@walley.org>
2017-04-18 13:45:01 +0200

    fixes oversized upload

--- guidepost.php
+++ guidepost.php
@@ -327,50 +327,51 @@ function insert_to_db($lat, $lon, $url ,$file, $author, $ref, $note, $license, $
 }
 
 ################################################################################
- # Returns an array of latitude and longitude from the Image file
- #   ---- http://stackoverflow.com/a/19420991 ----
- # @param image $file
- # @return multitype:number |boolean
-function read_gps_location($file){
+# Returns an array of latitude and longitude from the Image file
+#   ---- http://stackoverflow.com/a/19420991 ----
+# @param image $file
+# @return multitype:number |boolean
 ################################################################################
-    if (is_file($file)) {
-        $info = exif_read_data($file);
-        if (isset($info['GPSLatitude']) && isset($info['GPSLongitude']) &&
-            isset($info['GPSLatitudeRef']) && isset($info['GPSLongitudeRef']) &&
-            in_array($info['GPSLatitudeRef'], array('E','W','N','S')) && in_array($info['GPSLongitudeRef'], array('E','W','N','S'))) {
+function read_gps_location($file)
+{
+  if (is_file($file)) {
+    $info = exif_read_data($file);
+    if (isset($info['GPSLatitude']) && isset($info['GPSLongitude']) &&
+      isset($info['GPSLatitudeRef']) && isset($info['GPSLongitudeRef']) &&
+      in_array($info['GPSLatitudeRef'], array('E','W','N','S')) && in_array($info['GPSLongitudeRef'], array('E','W','N','S'))) {
 
-            $GPSLatitudeRef  = strtolower(trim($info['GPSLatitudeRef']));
-            $GPSLongitudeRef = strtolower(trim($info['GPSLongitudeRef']));
+      $GPSLatitudeRef  = strtolower(trim($info['GPSLatitudeRef']));
+      $GPSLongitudeRef = strtolower(trim($info['GPSLongitudeRef']));
 
-            $lat_degrees_a = explode('/',$info['GPSLatitude'][0]);
-            $lat_minutes_a = explode('/',$info['GPSLatitude'][1]);
-            $lat_seconds_a = explode('/',$info['GPSLatitude'][2]);
-            $lng_degrees_a = explode('/',$info['GPSLongitude'][0]);
-            $lng_minutes_a = explode('/',$info['GPSLongitude'][1]);
-            $lng_seconds_a = explode('/',$info['GPSLongitude'][2]);
+      $lat_degrees_a = explode('/',$info['GPSLatitude'][0]);
+      $lat_minutes_a = explode('/',$info['GPSLatitude'][1]);
+      $lat_seconds_a = explode('/',$info['GPSLatitude'][2]);
+      $lng_degrees_a = explode('/',$info['GPSLongitude'][0]);
+      $lng_minutes_a = explode('/',$info['GPSLongitude'][1]);
+      $lng_seconds_a = explode('/',$info['GPSLongitude'][2]);
 
-            $lat_degrees = $lat_degrees_a[0] / $lat_degrees_a[1];
-            $lat_minutes = $lat_minutes_a[0] / $lat_minutes_a[1];
-            $lat_seconds = $lat_seconds_a[0] / $lat_seconds_a[1];
-            $lng_degrees = $lng_degrees_a[0] / $lng_degrees_a[1];
-            $lng_minutes = $lng_minutes_a[0] / $lng_minutes_a[1];
-            $lng_seconds = $lng_seconds_a[0] / $lng_seconds_a[1];
+      $lat_degrees = $lat_degrees_a[0] / $lat_degrees_a[1];
+      $lat_minutes = $lat_minutes_a[0] / $lat_minutes_a[1];
+      $lat_seconds = $lat_seconds_a[0] / $lat_seconds_a[1];
+      $lng_degrees = $lng_degrees_a[0] / $lng_degrees_a[1];
+      $lng_minutes = $lng_minutes_a[0] / $lng_minutes_a[1];
+      $lng_seconds = $lng_seconds_a[0] / $lng_seconds_a[1];
 
-            $lat = (float) $lat_degrees+((($lat_minutes*60)+($lat_seconds))/3600);
-            $lng = (float) $lng_degrees+((($lng_minutes*60)+($lng_seconds))/3600);
+      $lat = (float) $lat_degrees+((($lat_minutes*60)+($lat_seconds))/3600);
+      $lng = (float) $lng_degrees+((($lng_minutes*60)+($lng_seconds))/3600);
 
-            //If the latitude is South, make it negative.
-            //If the longitude is West, make it negative
-            $GPSLatitudeRef  == 's' ? $lat *= -1 : '';
-            $GPSLongitudeRef == 'w' ? $lng *= -1 : '';
+      //If the latitude is South, make it negative.
+      //If the longitude is West, make it negative
+      $GPSLatitudeRef  == 's' ? $lat *= -1 : '';
+      $GPSLongitudeRef == 'w' ? $lng *= -1 : '';
 
-            return array(
-                'lat' => $lat,
-                'lng' => $lng
-            );
-        }
+      return array(
+        'lat' => $lat,
+        'lng' => $lng
+      );
     }
-    return false;
+  }
+  return false;
 }
 
 ################################################################################
@@ -651,6 +652,16 @@ $create_query = "CREATE TABLE changes (
 
 }
 
+
+################################################################################
+# START
+################################################################################
+
+if(empty($_FILES) && empty($_POST) && isset($_SERVER['REQUEST_METHOD']) && strtolower($_SERVER['REQUEST_METHOD']) == 'post') { //catch file overload error...
+  $post_max = ini_get('post_max_size'); //grab the size limits...
+  printdebug("file too too big".$post_max);
+  die("too big\n");
+}
 
 $action = get_param("action");
 
